5fc51186693d48129752ccdc24eafbfa2e7f25fa
leandrim1 <leandrimrodrigues532@gmail.com>
2025-03-10 17:52:22 -0300

    actualy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  server: { port: 3000 },
+});
 
 
 
